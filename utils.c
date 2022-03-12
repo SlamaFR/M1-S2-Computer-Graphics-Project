5e3856ae8ea6bc9965a7ab6a80544b1c3a0dd462
Irwin Madet <irwin.madet@gmail.com>
2022-03-12 18:03:01 +0100

    Utility methods

---
+++ utils.c
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+void g3x_NormalVertex3dv(Shape *shape, int i) {
+    g3x_Normal3dv(shape->normals[i]);
+    g3x_Vertex3dv(shape->vertexes[i]);
+}
---
+++ utils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <g3x.h>
+#include "shapes/shapes.h"
+
+int min(int a, int b);
+
+int max(int a, int b);
+
+void g3x_NormalVertex3dv(Shape *shape, int i);
